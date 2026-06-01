60fd8cd8537468720a260c36f52db4da6af6d509
lithium-ion111 <kasumi.2606@gmail.com>
2026-06-01 14:43:49 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,9 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   HOME_BANNER: process.env.NEXT_PUBLIC_HOME_BANNER || false, // ホームのバナーを非表示にする
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'ホワサバ勉強会保管庫', // 作者简介
+  AUTHOR: 'ホワサバ勉強会保管庫', // ←ここを好きな名前に
+  BIO: 'ホワサバ勉強会の保管庫です。最新の攻略情報をまとめています。', // ←紹介文
+  AVATAR: '', // アイコンを非表示にする
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-coral.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ホワイトアウトサバイバル, ホワサバ, 攻略, 勉強会', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
