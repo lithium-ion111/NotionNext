2f4fdcc0a0dc84bf235c37301b040b316a89a38f
lithium-ion111 <kasumi.2606@gmail.com>
2026-06-01 14:07:48 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,13 +19,14 @@ const BLOG = {
   HOME_BANNER: process.env.NEXT_PUBLIC_HOME_BANNER || false, // ホームのバナーを非表示にする
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'ホワサバ勉強会保管庫', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-coral.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ホワイトアウトサバイバル, ホワサバ, 攻略, 勉強会', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
-
+  // HEO主題固有設定：記事ページでトップメニューを表示するかどうか
+  HEO_MENU_DISPLAY: process.env.NEXT_PUBLIC_HEO_MENU_DISPLAY || true,
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能
 
