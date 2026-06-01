907c95149eafb72541c1d139812b3e6273dc67a5
lithium-ion111 <kasumi.2606@gmail.com>
2026-06-01 14:20:21 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   HOME_BANNER: process.env.NEXT_PUBLIC_HOME_BANNER || false, // ホームのバナーを非表示にする
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'ホワサバ勉強会保管庫', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-coral.vercel.app/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-coral.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ホワイトアウトサバイバル, ホワサバ, 攻略, 勉強会', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
